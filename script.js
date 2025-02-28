caf635ffb612d010a9f015417f47fa60220f233c
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-03-01 00:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let input = document.getElementsByClassName("input")[0];
+let btn = document.getElementsByClassName("btn")[0];
+
+btn.addEventListener("click", () => {
+	input.focus();
+})
